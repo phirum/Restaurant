128cc6898a962db1875d4cfc817a83cbe8ee8437
phirum <phirumsim@gmail.com>
2015-12-22 18:19:28 +0700

    add method

---
+++ pos/server/methods/test.js
@@ -0,0 +1,22 @@
+Meteor.methods({
+    insertCustomer: function () {
+        for (var i = 1; i <= 10000; i++) {
+            var customer = {};
+            customer._id = idGenerator.genWithPrefix(Pos.Collection.Customers, prefix, 6);
+            customer.name = "Name" + i;
+            customer.gender = "M";
+            customer.locationId = "001-002";
+            customer.branchId = "001";
+            customer._branch = {};
+            customer._branch.khName = "BB";
+            customer._branch.enName = "Battambang";
+            Pos.Collection.Customers.insert(customer);
+        }
+    },
+    insertProduct: function () {
+        for (var i = 1; i <= 10000; i++) {
+            var product = {};
+
+        }
+    }
+});
